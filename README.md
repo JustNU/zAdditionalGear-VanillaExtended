ee7aff220a1980b27ba9a6e0a3509e2151a15a1c
JustNU <ivancool108@gmail.com>
2023-01-03 08:27:22 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Additional Gear - Tan Module, Armored Vests
+Part of "Additional N" series of mods, requires my core. Vanilla like items using slighlty modified vanilla textures and/or meshes.
---
+++ src/mod.js
@@ -0,0 +1,133 @@
+﻿"use strict";
+
+class Mod
+{
+	
+	postDBLoad(container) 
+	{
+		// Constants
+		const logger = container.resolve("WinstonLogger");
+		const database = container.resolve("DatabaseServer").getTables();
+		const jsonUtil = container.resolve("JsonUtil");
+		const core = container.resolve("JustNUCore");
+		const modDb = `user/mods/zAdditionalGear-VanillaExtended/db/`;
+		const config = require("../config/config.json");
+		const itemConfig = require("../config/itemConfig.json");
+		const itemData = require("../db/items/itemData.json");
+		
+		// edge cases
+		const edgeCases = ["AddGearVanExt_GEN4_Light", "AddGearVanExt_6B43_Light"];
+		
+		//add retextures
+		for (const categoryId in itemConfig) {
+			for (const itemId in itemConfig[categoryId]) {
+				// skip edge cases, handle them later
+				if (edgeCases.includes(itemId)) {
+					continue;
+				}
+				
+				if (itemConfig[categoryId][itemId]) {
+					core.addItemRetexture(modDb, itemId, itemData[itemId].BaseItemID, itemData[itemId].BundlePath, config.EnableTradeOffers, config.AddToBots, itemData[itemId].LootWeigthMult);
+				}
+			}
+		}
+		
+		
+		// deal with edge cases
+		// GEN4 light
+		if (itemConfig["Armored Vests"]["AddGearVanExt_GEN4_Light"]) {
+			core.addItemRetexture(modDb, "AddGearVanExt_GEN4_Light", itemData["AddGearVanExt_GEN4_Light"].BaseItemID, itemData["AddGearVanExt_GEN4_Light"].BundlePath, false, config.AddToBots, itemData["AddGearVanExt_GEN4_Light"].LootWeigthMult);
+			
+			// change stats
+			const gen4Light = database.templates.items["AddGearVanExt_GEN4_Light"];
+			gen4Light._props.Weight = 10;
+			gen4Light._props.Height = 3;
+			gen4Light._props.Durability = Math.round(gen4Light._props.MaxDurability - (gen4Light._props.MaxDurability * 0.08));
+			gen4Light._props.MaxDurability = Math.round(gen4Light._props.MaxDurability - (gen4Light._props.MaxDurability * 0.08))
+			
+			if (gen4Light._props.speedPenaltyPercent != 0)
+				gen4Light._props.speedPenaltyPercent = gen4Light._props.speedPenaltyPercent + 1;
+			if (gen4Light._props.mousePenalty != 0)
+				gen4Light._props.mousePenalty = gen4Light._props.mousePenalty + 1;
+			if (gen4Light._props.weaponErgonomicPenalty != 0)
+				gen4Light._props.weaponErgonomicPenalty = gen4Light._props.weaponErgonomicPenalty + 1;
+			
+			// change price
+			database.templates.prices["AddGearVanExt_GEN4_Light"] = 85000;
+			
+			for (const handbookItemIndex in database.templates.handbook.Items) {
+				if (database.templates.handbook.Items[handbookItemIndex].Id === "AddGearVanExt_GEN4_Light") {
+					database.templates.handbook.Items[handbookItemIndex].Price = 85000;
+					break;
+				}
+			}
+			
+			// add trade offer
+			if (config.EnableTradeOffers)
+				core.createTraderOffer("AddGearVanExt_GEN4_Light", "5ac3b934156ae10c4430e83c", "5449016a4bdc2d6f028b456f", 90222, 3)
+		}
+		
+		// 6b43 light
+		if (itemConfig["Armored Vests"]["AddGearVanExt_6B43_Light"]) {
+			core.addItemRetexture(modDb, "AddGearVanExt_6B43_Light", itemData["AddGearVanExt_6B43_Light"].BaseItemID, itemData["AddGearVanExt_6B43_Light"].BundlePath, false, config.AddToBots, itemData["AddGearVanExt_GEN4_Light"].LootWeigthMult);
+			
+			// change stats
+			const sixB43Light = database.templates.items["AddGearVanExt_6B43_Light"];
+			sixB43Light._props.Weight = 14;
+			sixB43Light._props.Width = 3;
+			sixB43Light._props.Height = 3;
+			sixB43Light._props.Durability = Math.round(sixB43Light._props.MaxDurability - (sixB43Light._props.MaxDurability * 0.37));
+			sixB43Light._props.MaxDurability = Math.round(sixB43Light._props.MaxDurability - (sixB43Light._props.MaxDurability * 0.37))
+			
+			if (sixB43Light._props.speedPenaltyPercent != 0)
+				sixB43Light._props.speedPenaltyPercent = sixB43Light._props.speedPenaltyPercent + 3;
+			if (sixB43Light._props.mousePenalty != 0)
+				sixB43Light._props.mousePenalty = sixB43Light._props.mousePenalty + 3;
+			if (sixB43Light._props.weaponErgonomicPenalty != 0)
+				sixB43Light._props.weaponErgonomicPenalty = sixB43Light._props.weaponErgonomicPenalty + 3;
+			
+			// change price
+			database.templates.prices["AddGearVanExt_6B43_Light"] = 85000;
+			
+			for (const handbookItemIndex in database.templates.handbook.Items) {
+				if (database.templates.handbook.Items[handbookItemIndex].Id === "AddGearVanExt_6B43_Light") {
+					database.templates.handbook.Items[handbookItemIndex].Price = 85000;
+					break;
+				}
+			}
+			
+			// add trade offer
+			if (config.EnableTradeOffers)
+				core.createTraderOffer("AddGearVanExt_6B43_Light", "5ac3b934156ae10c4430e83c", "5449016a4bdc2d6f028b456f", 90222, 3)
+		}
+		
+		// Modify quests
+		if (config.EnableQuestChanges) {
+			const armoredVests = [
+				["AddGearVanExt_GEN4_Light"]
+			];
+			
+			// The survivalist path. Unprotected, but dangerous
+			if (database.templates.quests["5d25aed386f77442734d25d2"]) {
+				const unprotectedButDangerousGear = database.templates.quests["5d25aed386f77442734d25d2"].conditions.AvailableForFinish[0]._props.counter.conditions[1]._props.equipmentExclusive;
+				
+				database.templates.quests["5d25aed386f77442734d25d2"].conditions.AvailableForFinish[0]._props.counter.conditions[1]._props.equipmentExclusive = [
+					...jsonUtil.clone(unprotectedButDangerousGear),
+					...armoredVests
+				];
+			}
+			
+			// Swift one
+			if (database.templates.quests["60e729cf5698ee7b05057439"]) {
+				const swiftOneGear = database.templates.quests["60e729cf5698ee7b05057439"].conditions.AvailableForFinish[0]._props.counter.conditions[1]._props.equipmentExclusive;
+				
+				database.templates.quests["60e729cf5698ee7b05057439"].conditions.AvailableForFinish[0]._props.counter.conditions[1]._props.equipmentExclusive = [
+					...jsonUtil.clone(swiftOneGear),
+					...armoredVests
+				];
+			}
+		}
+	}
+}
+
+module.exports = { mod: new Mod() }
