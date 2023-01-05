d6f49f5812bde427ce349aaa0106b356cc21cbd8
JustNU <ivancool108@gmail.com>
2023-01-06 01:52:00 +0300

    move locale from core mod, add replace locale functionality

--- src/mod.js
+++ src/mod.js
@@ -10,10 +10,12 @@ class Mod
 		const database = container.resolve("DatabaseServer").getTables();
 		const jsonUtil = container.resolve("JsonUtil");
 		const core = container.resolve("JustNUCore");
-		const modDb = `user/mods/zAdditionalGear-VanillaExtended/db/`;
+		const VFS = container.resolve("VFS");
+		const modDb = `user/mods/zAdditionalGear-VanillaExtended/db`;
 		const config = require("../config/config.json");
 		const itemConfig = require("../config/itemConfig.json");
 		const itemData = require("../db/items/itemData.json");
+		const enLocale = jsonUtil.deserialize(VFS.readFile(`${modDb}/locales/en.json`));
 		
 		// edge cases
 		const edgeCases = [
@@ -25,14 +27,52 @@ class Mod
 			"AddGearVanExt_Defender2_Light"
 		];
 		
-		//add retextures
+		// add retextures
 		for (const categoryId in itemConfig) {
 			for (const itemId in itemConfig[categoryId]) {
+				
+				// handle locale
+				for (const localeID in database.locales.global) {
+					
+					// en placeholder
+					if (enLocale[itemId].Name)
+						database.locales.global[localeID][`${itemId} Name`] = enLocale[itemId].Name;
+					if (enLocale[itemId].ShortName)
+						database.locales.global[localeID][`${itemId} ShortName`] = enLocale[itemId].ShortName;
+					if (enLocale[itemId].Description)
+						database.locales.global[localeID][`${itemId} Description`] = enLocale[itemId].Description;
+					
+					// actual locale
+					if (VFS.exists(`${modDb}/locales/${localeID}.json`) && localeID != "en") {
+						const actualLocale = jsonUtil.deserialize(VFS.readFile(`${modDb}/locales/${localeID}.json`));
+
+						if (actualLocale[itemId].Name)
+							database.locales.global[localeID][`${itemId} Name`] = actualLocale[itemId].Name;
+						if (actualLocale[itemId].ShortName)
+							database.locales.global[localeID][`${itemId} ShortName`] = actualLocale[itemId].ShortName;
+						if (actualLocale[itemId].Description)
+							database.locales.global[localeID][`${itemId} Description`] = actualLocale[itemId].Description;
+					}
+					
+					// replace some default locale
+					if (VFS.exists(`${modDb}/localesReplace/${localeID}.json`)) {
+						const replaceLocale = jsonUtil.deserialize(VFS.readFile(`${modDb}/localesReplace/en.json`));
+						
+						for (const localeItem in replaceLocale) {
+							for (const localeItemEntry in replaceLocale[localeItem]) {
+								database.locales.global[localeID][`${localeItem} ${localeItemEntry}`] = replaceLocale[localeItem][localeItemEntry];
+							}
+						}
+					}
+					
+				}
+				
 				// skip edge cases, handle them later
 				if (edgeCases.includes(itemId)) {
 					continue;
 				}
 				
+				// add item retexture that is 1:1 to original item
 				if (itemConfig[categoryId][itemId]) {
 					core.addItemRetexture(modDb, itemId, itemData[itemId].BaseItemID, itemData[itemId].BundlePath, config.EnableTradeOffers, config.AddToBots, itemData[itemId].LootWeigthMult);
 				}
