f2d6a15049db8fa9b69fd67d524d9e65f648e39e
JustNU <ivancool108@gmail.com>
2023-08-02 01:58:50 +0300

    replace modpath

--- src/mod.js
+++ src/mod.js
@@ -11,12 +11,11 @@ class Mod
 		const jsonUtil = container.resolve("JsonUtil");
 		const core = container.resolve("JustNUCore");
 		const VFS = container.resolve("VFS");
-		const modLoader = container.resolve("PreAkiModLoader");
 		const config = require("../config/config.json");
 		const itemConfig = require("../config/itemConfig.json");
 		const itemData = require("../db/items/itemData.json");
 		const enLocale = require(`../db/locales/en.json`);
-		const modPath = modLoader.getModPath("AdditionalGear - Vanilla Extended");
+		const modPath = __dirname.split("\\").slice(0, -1).join("\\");
 		
 		// custom items
 		const customItems = [
@@ -52,7 +51,7 @@ class Mod
 						}
 					}
 					// actual locale
-					if (VFS.exists(`${modPath}locales/${localeID}.json`) && localeID != "en") {
+					if (VFS.exists(`${modPath}locales\\${localeID}.json`) && localeID != "en") {
 						const actualLocale = require(`../locales/${localeID}.json`);
 
 						if (actualLocale[itemId]) {
@@ -63,7 +62,7 @@ class Mod
 					}
 					
 					// replace some default locale
-					if (VFS.exists(`${modPath}localesReplace/${localeID}.json`)) {
+					if (VFS.exists(`${modPath}localesReplace\\${localeID}.json`)) {
 						const replaceLocale = require(`../localesReplace/${localeID}.json`);
 						
 						for (const localeItem in replaceLocale) {
